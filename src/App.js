451db772143c1255bf6d1abffd42eacd19f37ef3
Ricardo Granvilla <rgranvilla@gmail.com>
2020-08-29 23:09:56 -0300

    Solução desafio 04

--- src/App.js
+++ src/App.js
@@ -54,6 +54,7 @@ export default function App() {
                   </Text>
                 ))}
               </SafeAreaView>
+              
               <SafeAreaView style={styles.likesContainer}>
                 <Text
                   style={styles.likeText}
